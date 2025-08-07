37c9d6d9a8fbcf69f4ca018ef070a07b6282c9f1
Julioklyn <julio.carlos@aluno.ifsp.edu.br>
2025-08-07 17:36:41 -0300

    projeto

---
+++ src/components/Topo/index.js
@@ -0,0 +1,15 @@
+export default function Topo() {
+    <header>
+        <div>
+            <picture>
+            <a href=""><img src="./logo01.svg"/></a>
+            </picture>
+
+            <nav>
+                <a href="#">Produtos</a>
+                <a href="#">Sobre</a>
+                <a href="#">Contato</a>
+            </nav>
+        </div>
+    </header>
+}
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import Topo from "@/components/Topo";
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="loja de Relógios" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Topo/>
+
+    </>
+  );
+}
